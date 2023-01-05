0f9e13aa8d4bf923948157b0a5eb6acd4e58a3df
crowdbotics-dev <engineering@crowdbotics.com>
2023-01-05 10:31:41 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Addressmbrowjawgt
+admin.site.register(Addressmbrowjawgt)
 
 # Register your models here.
--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Addressmbrowjawgt
+from .serializers import AddressmbrowjawgtSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,8 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+class AddressmbrowjawgtViewSet(viewsets.ModelViewSet):
+    serializer_class = AddressmbrowjawgtSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    queryset = Addressmbrowjawgt.objects.all()
